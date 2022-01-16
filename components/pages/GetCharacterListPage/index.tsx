bcb4ec3e7aa539c44c133c164770a49cea286dd3
williamjoseph77 <williamblack56@gmail.com>
2022-01-16 15:55:46 +0700

    refactor: handle empty character list

--- components/pages/GetCharacterListPage/index.tsx
+++ components/pages/GetCharacterListPage/index.tsx
@@ -44,32 +44,41 @@ const GetCharacterListPage: NextPage = () => {
         <meta name="description" content="Character List" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <table className={styles.characterListTableContainer}>
-        <tbody>
-          <tr>
-            <td colSpan={4} className={styles.formTitleContainer}>
-              Character List
-            </td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Role</td>
-            <td>Power</td>
-            <td>Wealth</td>
-          </tr>
-          {characters?.map((character) => {
-            // if there's detail page, then use character guid as url query, and use Link component
-            return (
-              <tr key={character.guid}>
-                <td>{character.name}</td>
-                <td>{character.role.name}</td>
-                <td>{character.power}</td>
-                <td>{character.wealth}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {!characters ? (
+        <div className={styles.emptyContainer}>
+          Belum ada data <br />
+          Buat Character baru dengan mengisi form data Character Anda <br />
+          Untuk mengakses halamannya, silakan klik button Create Character
+          Secure/Non Secure <br />
+        </div>
+      ) : (
+        <table className={styles.characterListTableContainer}>
+          <tbody>
+            <tr>
+              <td colSpan={4} className={styles.formTitleContainer}>
+                Character List
+              </td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>Role</td>
+              <td>Power</td>
+              <td>Wealth</td>
+            </tr>
+            {characters?.map((character) => {
+              // if there's detail page, then use character guid as url query, and use Link component
+              return (
+                <tr key={character.guid}>
+                  <td>{character.name}</td>
+                  <td>{character.role.name}</td>
+                  <td>{character.power}</td>
+                  <td>{character.wealth}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
